3950a7de87c227d6932b9c1ebebba3842684b6ac
Nakulkomarla <nakulkolle@gmail.com>
2016-05-25 19:55:35 +0530

    Add files via upload

---
+++ assignment2.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b=4,c;
+		System.out.println("value of b:"+b);
+
+	}
+
+}
